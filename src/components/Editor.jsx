62e1821e651d2a7bc918fb1d3c3602cb2f1b78a3
andrewbaas <61301494+andrewbaas@users.noreply.github.com>
2025-12-18 11:29:40 -0600

    Cleaning up unnecssary leftovers

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -21,7 +21,7 @@ export default function Editor({highlights, setHighlights, setHover, defaultValu
       hl,
     ]);
   }
-  
+
   const addHighlightRef = useRef(addHighlight);
   
   useEffect(() => {
@@ -31,47 +31,6 @@ export default function Editor({highlights, setHighlights, setHover, defaultValu
       updateHoverFromState(hl.id, hl.hover);
     });
   }, [highlights])
-
-/*  const onTextChangeRef = useRef(onTextChange);
-  const onSelectionChangeRef = useRef(onSelectionChange);
-
-  useLayoutEffect(() => {
-    onTextChangeRef.current = onTextChange;
-    onSelectionChangeRef.current = onSelectionChange;
-  });*/
-/*
-  useEffect(() => {
-    ref.current?.enable(!readOnly);
-  }, [ref, readOnly]);*/
-/*
-  useEffect(() => {
-    const container = containerRef.current;
-    const editorContainer = container.appendChild(
-      container.ownerDocument.createElement('div'),
-    );
-    const quill = new Quill(editorContainer, {
-      theme: 'snow',
-    });
-
-    ref.current = quill;
-
-    if (defaultValueRef.current) {
-      quill.setContents(defaultValueRef.current);
-    }
-
-    quill.on(Quill.events.TEXT_CHANGE, (...args) => {
-      onTextChangeRef.current?.(...args);
-    });
-
-    quill.on(Quill.events.SELECTION_CHANGE, (...args) => {
-      onSelectionChangeRef.current?.(...args);
-    });
-
-    return () => {
-      ref.current = null;
-      container.innerHTML = '';
-    };
-  }, [ref]);*/
 
   useEffect(() => {
     const container = containerRef.current
